1a87d291338294884b2d7773c85ab2ff8133c3b4
πόσο σ' αγαπώ <1192338674@qq.com>
2022-05-14 20:39:42 +0800

    error fix
    error fix

--- pre_train_onmt/transforms/__init__.py
+++ pre_train_onmt/transforms/__init__.py
@@ -48,4 +48,4 @@ for file in os.listdir(transform_dir):
             file.endswith('.py') or os.path.isdir(path)):
         file_name = file[:file.find('.py')] if file.endswith('.py') else file
         module = importlib.import_module(
-            'onmt.transforms.' + file_name)
+            'pre_train_onmt.transforms.' + file_name)
